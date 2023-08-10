25493cdf138c8365b73079cdf836ce45d701c6e8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-10 13:03:11 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using printf command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+        pritf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
