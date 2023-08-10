e41b558bca475933ed861bb264ce9d7adbbc0e7f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-10 12:22:30 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #!/bin/bash
-/*
- *a C program that prints exactly "Programming is like building a multilingual puzzle"
- */
 #include <stdio.h>
-
 int main()
 {
         puts("Programming is like building a multilingual puzzle");
