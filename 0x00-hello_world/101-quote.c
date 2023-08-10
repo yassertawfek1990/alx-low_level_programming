3f3efdae6294b72d073d5981a0522b0e5c3d6e97
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-10 15:02:44 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char *errormessage = "and that piece of art is useful:\" - Dora Korpar, 2015-10-19\n"
+	char *errormessage = "and that piece of art is useful:\" - Dora Korpar, 2015-10-19\n";
 	write(2, errormessage, strlen(errormessage));
         return (1);
 }
