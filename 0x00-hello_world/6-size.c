02d553a685c7a181b0fafb12ac9b160185763750
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-10 13:21:58 -0700

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints the size of various types
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	printf("Size of a char", sizeof(char));
+	printf("Size of a int", sizeof(int));
+	printf("Size of a long int", sizeof(long int));
+	printf("Size of a long long int", sizeof(long long int));
+	printf("Size of a float", sizeof(float));
+	return (0);
+}
