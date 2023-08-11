7e6c6b7bd1df3b1ca64f2ba53819479ed5c4a846
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 13:37:33 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	int da = 48;
+
+	while (da <= 57)
+	{
+		putchar(da);
+		if (da < 57)
+			putchar(',');
+			putchar(' ');	
+		da++;
+	}
+	putchar('\n');
+	return (0);
+}
