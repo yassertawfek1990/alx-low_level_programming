c420fff159a3fe8e4207b98e3558f02ed552d74f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 12:44:36 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using printf command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	char da = "a";
+
+	while (da <= 'z')
+	{
+		if (da == "e" || da == "q")
+			char++;
+		putchar(da);
+		char++;
+	}
+	putchar('\n');
+	return (0)
+}
