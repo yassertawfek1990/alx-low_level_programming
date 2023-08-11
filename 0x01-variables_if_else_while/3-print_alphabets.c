dabed76ef3fa87f25c5f6c4e418c18beabd66243
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 12:27:56 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using while command
+ *
+ * Return: always return 0
+*/
+int main(void)
+{
+	char da = 'a';
+	char cap = 'A';
+
+	while (da <= 'z')
+	{
+		putchar(da);
+		da++;
+	}
+	while (cap <= 'Z')
+	{
+		putchar(cap);
+		da++;
+	}
+	putchar('\n');
+	return (0);
+}
