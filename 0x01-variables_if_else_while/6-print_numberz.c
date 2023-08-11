dd937f4c6f413706b76295b7f4c64916ecd3caa5
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 13:02:03 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+        int da = 48;
+
+        while (da <= 57)
+        {
+                putchar(da);
+                da++;
+        }
+        putchar('\n');
+        return (0);
+}
