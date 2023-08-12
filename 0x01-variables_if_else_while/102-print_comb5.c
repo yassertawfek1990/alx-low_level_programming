9421c1aed483339c6cf27d51905b9568b59609c6
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 19:56:30 -0700

    c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	int d1 = 0;
+	int d2;
+
+	while (d1 <= 99)
+	{
+		d1 = d2;
+		while (d2 <= 99)
+		{
+			if (d1 != d2)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+
+				if (d1 != 98 || d2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
