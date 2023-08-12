0d5f2017449f011b7d98405a136534da398334f4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-11 19:47:20 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,23 +10,29 @@
 
 int main(void)
 {
-	int d1 = 0, d2;
+	int d1 = 0, d2, d3;
 
 	while (d1 <= 9)
 	{
 		d2 = 0;
 		while (d2 <= 9)
 		{
-			if (d1 != d2 && d1 < d2)
-			{
-				putchar(d1 + 48);
-				putchar(d2 + 48);
-
-				if (d1 + d2 != 17)
+			d3 = 0;
+			while (d3 <= 9)
+			{	
+				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(d1 + 48);
+					putchar(d2 + 48);
+					putchar(d3 + 48);
+
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}	
 				}
+				d3++
 			}
 			d2++;
 		}
