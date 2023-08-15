99878cb8b5ea21188faf020be422016210e80424
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 16:06:23 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-        for (h = 0; h <= 23; h++)
-	{	
+	for (h = 0; h <= 23; h++)
+	{
 		for (m = 0; m <= 60; m++)
-		{	_putchar(h / 10 + 48);
-			_putchar(h % 10 + 48);
+		{	_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
 			_putchar(':');
-			_putchar(m / 10 + 48);
-			_putchar(m / 10 + 48);
+			_putchar((m / 10) + 48);
+			_putchar((m / 10) + 48);
 			_putchar('\n');
 		}
 	}
