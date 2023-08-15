8f13decd3e39b3755b052929b5a5089963260af4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 05:51:19 -0700

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using printf command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
+
