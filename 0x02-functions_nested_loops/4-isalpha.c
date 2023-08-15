9b7be5ff45d0ff9c54c5a9ec64cf15abb27f035a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 14:22:37 -0700

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/**
+ * _isalpha - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @c: character
+ *
+ * Return: 1 if lowercase 0 if uppercase
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1)
+	if (c >= 97 && c <= 122)
+		return (1);
+
+	return (0);
+
+}	
