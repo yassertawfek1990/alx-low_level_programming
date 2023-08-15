b1f1ceff5a770ebbaf66154e0e727e50fd72fd74
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 16:41:51 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @a: 1st
+ * @b:2nd
+ *
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+
+}
+
