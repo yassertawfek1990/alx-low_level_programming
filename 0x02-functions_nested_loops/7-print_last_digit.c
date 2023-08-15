3033683dc8af7c384dd56498a613a2ca64509b69
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 15:29:15 -0700

    c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @r: integer
+ *
+ * Return: 1 if lowercase 0 if uppercase
+*/
+
+int print_last_digit(int r)
+{
+	int d;
+	d = r % 10;
+
+	return (d);
+
+}
