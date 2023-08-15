b76da039c3661e9bafe4887155bb4e720ee978b8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 14:38:57 -0700

    c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * print_sign - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @n: integer
+ *
+ * Return: 1 if positive or negative 0 if zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(43);
+		return (1);
+
+	else if (n = 0)
+		_putchar(48);
+		return (0);
+
+	else (n < 0)
+		_putchar(45);
+		return (-1);
+
+}
