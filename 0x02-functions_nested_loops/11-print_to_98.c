82f60154bd2e7273163be6bc12866d3142a8ca53
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 16:57:30 -0700

    c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @n: number
+ *
+ * 
+*/
+
+int print_to_98(int n)
+{
+	int c;
+
+	if (n < 98)
+		for (c = n; c < 98; c++)
+			printf("%d, ", c)
+	else 			
+		for (c = n; c > 98; c++)
+			printf("%d, ", c)
+	printf("98\n")
+
+}
