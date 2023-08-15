85a53f981a55052078a4bcdf52af856af8532573
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 14:06:25 -0700

    c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+*/
+
+int _islower(int c);
+{
+	if (c >=97 && c <= 122)
+		return (1);
+
+	return (0);
+
+}
