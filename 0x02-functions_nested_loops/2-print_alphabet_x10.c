a86bf888462d5d3a03c6f4af6da0a325daacfadc
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 13:52:24 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets using asci code
+ * print lowercase alphabet a-z
+*/
+
+void print_alphabet_x10(void);
+{
+        int num 
+	for (num = 1; num <= 10; num++)
+
+		int alph;
+
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+
+        	_putchar('\n');
+
+}
