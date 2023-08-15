2c2416108ffee19422b323c2fc606b3becf831dd
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 15:17:57 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * __abs - print alphabets if lowercase using asci code
+ * print lowercase alphapet a-z
+ *
+ * @c: character
+ *
+ * Return: 1 if lowercase 0 if uppercase
+*/
+
+int _abs(int r)
+{
+	if (r < 0)
+		return (r * (-1));
+
+}
