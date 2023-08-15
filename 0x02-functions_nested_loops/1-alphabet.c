49bdccbb54c1d8b798149bd30c0f9ad6feb4e6b0
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 13:44:21 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet: print alphabets using asci code
+ * print_alphabet - print alphabets using asci code
  * print lowercase alphabet a-z
 */
 
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	int alph;
 
-	for (alph = 'a'; alph <= 'z'; a++)
+	for (alph = 'a'; alph <= 'z'; alph++)
 		_putchar(alph);
 
 	_putchar('\n')
