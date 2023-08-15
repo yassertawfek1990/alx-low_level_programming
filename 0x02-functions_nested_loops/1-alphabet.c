f29c96cdeaea18bfed7d0dd9bb7649468cc3fcba
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 13:39:37 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet: print alphabets using asci code
+ * print lowercase alphabet a-z
+*/
+
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; a++)
+		_putchar(alph);
+
+	_putchar('\n')
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@
 
 
 int _putchar(char x);
+void print_alphabet(void);
 
 #endif
