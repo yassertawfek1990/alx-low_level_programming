0d95b5608271f044791adc24be683d9775dde603
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 19:02:08 -0700

    c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using printf command
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	unsigned long f1 = 0;
+	unsigned long f2 = 1;
+	unsigned long s;
+	float t;
+
+	while (1)
+	{	
+
+		s = f1 + f2;
+
+		if (s > 4000000)
+			break;
+
+		if ((s % 2) == 0)
+			t += sum;
+
+		f1 = f2;
+		f2 = s;
+	}
+	printf("%.0f\n", t);
+
+	return (0);
+}
