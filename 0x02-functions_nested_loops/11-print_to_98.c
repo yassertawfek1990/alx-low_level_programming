4fcdcb792d4dada4ed67216d488834b1ce5c66ed
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 17:03:24 -0700

    c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,8 +5,6 @@
  * print lowercase alphapet a-z
  *
  * @n: number
- *
- * 
 */
 
 void print_to_98(int n)
@@ -16,8 +14,8 @@ void print_to_98(int n)
 	if (n < 98)
 		for (c = n; c < 98; c++)
 			printf("%d, ", c);
-	else 			
-		for (c = n; c > 98; c++)
+	else
+		for (c = n; c > 98; c--)
 			printf("%d, ", c);
 	printf("98\n");
 
