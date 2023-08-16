4058a48b90addd75fdd8b29b8191bd08e1e66450
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-15 18:00:35 -0700

    c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,8 +13,8 @@ int main(void)
 	int n;
 	int s;
 
-	for (n = 0; n <1024; n++)
-		if ((n % 3 == 0) && (n % 5 == 0))
+	for (n = 0; n < 1024; n++)
+		if ((n % 3 == 0) || (n % 5 == 0))
 			s += n;
 	printf("%d\n", s);
 
