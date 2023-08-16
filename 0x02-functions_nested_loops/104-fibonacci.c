64f55d9e5ff8973f09fda0a82a52f727edc2603f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-16 16:23:56 -0700

    c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,7 +38,7 @@ int main(void)
 	int i;
 	unsigned long n1 = 1;
 	unsigned long n2 = 2;
-	unsigned long max = 100000000;
+	unsigned long maxi = 100000000;
 	unsigned long n01 = 0;
 	unsigned long n02 = 0;
 	unsigned long t;
@@ -48,5 +48,28 @@ int main(void)
 	{
 		if (n01 > 0)
 			pritf("%lu", n01);
-			i = len(max) - 1 - len(n1)l
+		i = len(maxi) - 1 - len(n1);
+
+		while (n01 > 0 && i > 0)
+		{
+			printf("%d", 0);
+			i--;
+		}
+
+		printf("%lu", n1);
+
+		t = (n1 + n2) % maxi;
+		t0 = (n01 + n02) + (n1 + n2) / maxi;
+		n1 = n2;
+		n01 = n02;
+		n2 = t;
+		n01 = t0;
+
+		if (c != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
 }
