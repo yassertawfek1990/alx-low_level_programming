efb338e48c9cc8d4763efbbf65fef7847d9bf619
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 14:30:06 -0700

    c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Enter program
+ *
+ * Description: C program prints using printf command
+*/
+
+void main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((n % 3) == 0)
+			printf("Fizz ");
+		else if ((n % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+}
+
