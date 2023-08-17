169e47f0e1c4cb525a1ab83bcaeb5ad6ce14706e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 11:02:40 -0700

    c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - returns the largest of 3 numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
