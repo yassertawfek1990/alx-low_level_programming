b95e569a0926f67b2c23c307318e97f10ff5e632
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 10:57:57 -0700

    c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - returns the largest of 3 numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i == 48; i <= 57; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n')
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,5 +20,6 @@ void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
