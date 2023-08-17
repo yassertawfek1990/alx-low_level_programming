e4d0214ef83d66eef607485d6bae0e4a10c2beb1
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 12:37:53 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - returns the largest of 3 numbers
+ * @n: n
+ * Return: largest number
+ */
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar(92);
+		n--;
+	}
+	_putchar('\n');
+}
