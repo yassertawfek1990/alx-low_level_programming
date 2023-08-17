4088eaf5332cb9354994bc0f7d59c8404cee9175
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 16:51:46 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,41 +9,41 @@
 
 void print_number(int n)
 {
-        if (n <= -10 && n > -100)
-        {
-                n = n * -1;
-                _putchar(45);
-                _putchar((n / 10) + 48);
-                _putchar((n % 10) + 48);
-        }
-        else if (n < 0 && n > -10)
-        {
-                n = n * -1;
-                _putchar(45);
-                _putchar(n + 48);
-        }
-        else if (n > 0 && n < 10)
-                _putchar(n + 48);
-        else if (n >= 10 && n < 100)
-        {
-                _putchar((n / 10) + 48);
-                _putchar((n % 10) + 48);
-        }
-        else if (n >= 100 && n < 1000)
-        {
-                _putchar((n / 100) + 48);
-                _putchar(((n / 10) % 10) + 48);
-                _putchar((n % 10) + 48);
-        }
-        else if (n >= 1000 && n < 10000)
-        {
-                _putchar((n / 1000) + 48);
-                _putchar(((n / 100) % 10) + 48);
-                _putchar(((n / 10) % 10) + 48);
-                _putchar((n % 10) + 48);
-        }
-        else
-                _putchar(48);
+	if (n <= -10 && n > -100)
+	{
+		n = n * -1;
+		_putchar(45);
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n < 0 && n > -10)
+	{
+		n = n * -1;
+		_putchar(45);
+		_putchar(n + 48);
+	}
+	else if (n > 0 && n < 10)
+		_putchar(n + 48);
+	else if (n >= 10 && n < 100)
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar((n / 100) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar((n / 1000) + 48);
+		_putchar(((n / 100) % 10) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else
+		_putchar(48);
 }
 
 
