3b961f5da5b279f261fd2c6ce2a83c9ba0f920a7
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 16:48:03 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,44 @@
  * Return: largest number
  */
 
-int print_number(int n)
+
+void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = n * -1;
-	}
-
-	if (n / 10 != 0)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+        if (n <= -10 && n > -100)
+        {
+                n = n * -1;
+                _putchar(45);
+                _putchar((n / 10) + 48);
+                _putchar((n % 10) + 48);
+        }
+        else if (n < 0 && n > -10)
+        {
+                n = n * -1;
+                _putchar(45);
+                _putchar(n + 48);
+        }
+        else if (n > 0 && n < 10)
+                _putchar(n + 48);
+        else if (n >= 10 && n < 100)
+        {
+                _putchar((n / 10) + 48);
+                _putchar((n % 10) + 48);
+        }
+        else if (n >= 100 && n < 1000)
+        {
+                _putchar((n / 100) + 48);
+                _putchar(((n / 10) % 10) + 48);
+                _putchar((n % 10) + 48);
+        }
+        else if (n >= 1000 && n < 10000)
+        {
+                _putchar((n / 1000) + 48);
+                _putchar(((n / 100) % 10) + 48);
+                _putchar(((n / 10) % 10) + 48);
+                _putchar((n % 10) + 48);
+        }
+        else
+                _putchar(48);
 }
+
 
