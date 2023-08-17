5debdc55925526a2d8bb416306c53658c5770978
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 12:31:41 -0700

    c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - returns the largest of 3 numbers
+ * @n: n
+ * Return: largest number
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar(45);
+		n--;
+	}
+	_putchar('\n');
+}
