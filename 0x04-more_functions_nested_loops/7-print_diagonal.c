f885d46fae8a15fe7be5c80795b5bb57de0ed7d2
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 13:30:39 -0700

    c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,10 +7,19 @@
  */
 void print_diagonal(int n)
 {
-	while (n > 0)
+	int i;
+	int c;
+
+	if (n > 0)
 	{
+		for (i = 1; i <= n; i++)
+		{
+			for (c = 1; c <= i; c++)
+				_putchar(' ');
 		_putchar(92);
-		n--;
+		_putchar('\n');
+		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
