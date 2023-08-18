3d37903355f5fcb50aadf9c03064637d458bfa23
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 19:08:43 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,9 +11,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n = -n;
 	}
-	if (n / 10 > 0)
+	if ((n / 10) > 0)
 		print_number(n / 10);
 	_putchar((n % 10) + 48);
 }
