a40494fefcb206e584462be2d85edc1f1299fcc2
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-17 17:34:46 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,23 +2,19 @@
 
 /**
  * print_number - returns the largest of 3 numbers
- * @n: number
+ * @n: first intege
  * Return: largest number
  */
 
-
 void print_number(int n)
 {
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
-
 	if (n / 10 > 0)
 		print_number(n / 10);
-
 	_putchar((n % 10) + '0');
 }
 
