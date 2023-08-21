755345e1a4a6068a1d4ff891803bbfc04d75beec
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 13:17:21 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - returns the largest of 3 numbers
+ * @s: first integ
+ */
+
+void print_rev(char *s);
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = l-1; i >= 0; i--)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
