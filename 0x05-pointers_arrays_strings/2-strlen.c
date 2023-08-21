703d7ba30b5bae76ec23551b0f2e2203f392ea83
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 12:45:07 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the largest of 3 numbers
+ * @s: first integer
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+
+	return (l);
+}	
