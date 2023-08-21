39b1d09f1c2b447e637c75ea8a374ee293c42021
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 16:48:50 -0700

    c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] < '\0'; i++)
+	do
+	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (src[i] != '\0');
 
 	return (dest);
 }
