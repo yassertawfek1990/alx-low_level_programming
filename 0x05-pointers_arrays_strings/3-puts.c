e4ad077c20fa008a07d072daf9d83af8d9f39486
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 13:09:04 -0700

    c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - returns the largest of 3 numbers
+ * @ste: first integ
+ */
+
+void _puts(char *str)
+{
+	int i;
+	int l = strlen(str);
+	
+	for (i = 0; i <= l; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
