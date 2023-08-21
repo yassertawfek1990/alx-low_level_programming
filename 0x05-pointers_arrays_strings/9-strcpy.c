ca067fdef22542ac9fbbee79734cd45ee5e390e1
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 16:22:11 -0700

    c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - returns the largest of 3 numbers
+ * @dest: first integer
+ * @src: second intege
+ * Return: largest number
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
