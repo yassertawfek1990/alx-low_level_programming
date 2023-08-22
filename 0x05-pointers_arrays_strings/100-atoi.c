e9b5de619737824bffc5a75ee1571482ff6a270a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-22 09:42:00 -0700

    c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,21 +10,22 @@ int _atoi(char *s)
 {
 	int m;
 	int x;
+
 	m = 1;
 	x = 0;
 
-	while (*s) 
+	while (*s)
 	{
 		if (*s == '-')
 			m = -1;
 		else if (*s >= '0' && *s <= '9')
 			x = x * 10 + (*s - '0');
-	
+
 		else if (x > 0)
 			break;
 	}
 	s++;
 
 
-	return x * m;
+	return (x * m);
 }
