60503504955b4b8bd024d13e37c0579100876c1b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-21 19:09:55 -0700

    c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,19 +10,31 @@
  * Return: always return 0
 */
 
-int main(void)
-{
-	int x;
-	char y;
-
-	srand(time(NULL));
-
-	while (x <= 2634)
-	{
-		y = rand() % 128;
-		x += y;
-		putchar(y);
-	}
-	putchar(2761 - x);
-	return (0);
+int random_int(int min, int max) {
+    return min + rand() % (max - min + 1);
 }
+
+// Function to generate a random valid password
+void generate_password(char *password) {
+    const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int random_index = random_int(0, NUM_VALID_CHARS - 1);
+        password[i] = valid_chars[random_index];
+    }
+
+    password[PASSWORD_LENGTH] = '\0';
+}
+
+int main() {
+    srand(time(NULL)); // Seed the random number generator with the current time
+
+    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    generate_password(password);
+
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
+
+
