852fd2cd3b554312e81b893bcfcacb842426ee74
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-22 08:44:37 -0700

    c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,31 +10,23 @@
  * Return: always return 0
 */
 
-int random_int(int min, int max) {
-    return min + rand() % (max - min + 1);
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    char password[6];
+
+    srand(time(NULL));
+    
+    password[0] = rand() % 26 + 'A';
+    password[1] = rand() % 26 + 'a';
+    password[2] = rand() % 10 + '0';
+    password[3] = rand() % 26 + 'A';
+    password[4] = rand() % 26 + 'a';
+    password[5] = '\0';
+
+    printf("%s", password);
+
+    return (0);
 }
-
-// Function to generate a random valid password
-void generate_password(char *password) {
-    const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        int random_index = random_int(0, NUM_VALID_CHARS - 1);
-        password[i] = valid_chars[random_index];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-}
-
-int main() {
-    srand(time(NULL)); // Seed the random number generator with the current time
-
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
-    generate_password(password);
-
-    printf("Generated Password: %s\n", password);
-
-    return 0;
-}
-
-
