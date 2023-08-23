9d39db1870b731be70646568426755fd5d93071c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-23 13:57:41 -0700

    c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - returns the largest of 3 numbers
+ * @src: first integer
+ * @dest: second integer
+ * @a: d
+ * Return: largest number
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int d;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (d = 0; d < n; d++)
+	{
+		dest[i] = src[d];
+		i++;
+	}
+
+	return (dest);
+}
