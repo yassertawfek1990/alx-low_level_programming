842e9003d10c00f1e612078642e6b68e95f592ef
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-23 15:56:38 -0700

    c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - returns the largest of 3 numbers
+ * @str: first integer
+ * Return: largest number
+ */
+
+char *string_toupper(char *str)
+{
+	int d;
+
+	for (d = 0; str[d] != '\n'; d++)
+	{
+		if (str[d] >= 97 && str[d] <= 122)
+			str[d] = (65 + (str[d] - 97));
+	}
+
+	return (str);
+}
