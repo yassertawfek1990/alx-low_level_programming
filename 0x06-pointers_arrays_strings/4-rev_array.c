a5023607c05ad610bbbcfd714a6a3c076c2eac25
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-24 05:06:03 -0700

    c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -19,13 +19,4 @@ void reverse_array(int *a, int n)
 		a[d] = a[n - d - 1];
 		a[n - d - 1] = x;
 	}
-	for (i = 0; i < n; i++)
-	{
-		if (i == n - 1)
-			printf("%d", a[i]);
-		else
-			printf("%d, ", a[i]);
-	}
-
-	printf("\n");
 }
