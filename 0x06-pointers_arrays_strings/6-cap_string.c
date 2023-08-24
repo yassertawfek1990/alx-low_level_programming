7a6dfb7e6e3f3728593ca7b81d8552bec9939e4c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-24 09:20:59 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,33 +8,33 @@
 
 char *cap_string(char *str)
 {
-        int i;
-        int x;
-        char d[] = " \t\n,;.!?\"(){}";
-        int l = strlen(str);
-        
-        for (i = 0; i < l && str[i] != '\0'; i++)
-        {
-            if (str[i + 1] != '\0')
-            {
-                for (x = 0; x <= 12; x++)
-                {
-                        if (str[i] == d[x])
-                        {
-                            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                            {
-                                str[i + 1] = (65 + (str[i + 1] - 97));
-                            }
-                        }
-                        else if (i == 0)
-                        {
-                            if (str[i] >= 97 && str[i] <= 122)
-                            {
-                                    str[i] = (65 + (str[i] - 97));
-                            }
-                        }
-                }
-            }
-       }
-        return (str);
+	int i;
+	int x;
+	char d[] = " \t\n,;.!?\"(){}";
+	int l = strlen(str);
+
+	for (i = 0; i < l && str[i] != '\0'; i++)
+	{
+		if (str[i + 1] != '\0')
+		{
+			for (x = 0; x <= 12; x++)
+			{
+				if (str[i] == d[x])
+				{
+					if (str[i + 1] >= 97 && str[i + 1] <= 122)
+					{
+						str[i + 1] = (65 + (str[i + 1] - 97));
+					}
+				}
+				else if (i == 0)
+				{
+					if (str[i] >= 97 && str[i] <= 122)
+					{
+						str[i] = (65 + (str[i] - 97));
+					}
+				}
+			}
+		}
+	}
+	return (str);
 }
