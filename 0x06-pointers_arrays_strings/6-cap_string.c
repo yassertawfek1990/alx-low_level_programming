85a2310430afc19b73807bd2e44194886a4d2582
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-24 08:08:08 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
 /**
- * string_toupper - returns the largest of 3 numbers
+ * cap_string - returns the largest of 3 numbers
  * @str: first integer
  * Return: largest number
  */
 
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
-	int d;
+	int i;
+	int x;
+	char d[] = " \t\n,;.!?\"(){}";
 
-	for (d = 0; d < strlen(str); d++)
+	for (i = 0; i < strlen(str); i++)
 	{
-		if (str[d] >= 97 && str[d] <= 122)
-			str[d] = (65 + (str[d] - 97));
+		for (x = 0; x <= 12; x++)
+		{
+			if (str[i] == d[x])
+			{
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] = (65 + (str[i + 1] - 97));
+				}
+			}
+		}
 	}
-
 	return (str);
 }
