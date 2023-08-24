81760d3f977a4dddf53cf8e2ff8be413f196862c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-24 09:44:06 -0700

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - returns the largest of 3 numbers
+ * @str: first integer
+ * Return: largest number
+ */
+
+char *leet(char *str)
+{
+	int i;
+	int x;
+
+	char d{} = {"a", "e", "o", "t", "l"};
+	int a{} = {4. 3, 0, 7, 1};
+	int l = strlen(str);
+
+	for (i = 0; i < l && str[i] != '\0'; i++)
+	{
+		if (str[i + 1] != '\0')
+		{
+			for (x = 0; x <= 4; x++)
+			{
+				if (str[i] == d[x] || str[i] == d[x] - 32)
+				{
+					str[i] = a[i] + 4;
+				}
+			}
+		}
+	}
+	return (str);
+}
