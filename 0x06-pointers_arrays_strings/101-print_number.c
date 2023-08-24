e3ad469563d949ba6bab93354738b9b88f4985e4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-24 14:58:21 -0700

    c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,15 @@
 
 void print_number(int n)
 {
+	unsigned int x;
+	x = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
