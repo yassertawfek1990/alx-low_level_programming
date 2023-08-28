c08ee754d623cb02e8c0c02ce352c1ff14234640
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-28 12:04:59 -0700

    c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - returns the largest of 3 numbers
+ * @dest: first intege
+ * @src:d
+ * @n:d
+ * Return: largest number
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -52,5 +52,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
