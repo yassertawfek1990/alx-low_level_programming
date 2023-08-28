e2b9a44dab71f0b90ae625171bf994a4ab83d79a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-28 16:24:23 -0700

    c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - returns the largest of 3 numbers
+ * @s: first intege
+ * @accept:d
+ * Return: largest number
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == accept[0)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
