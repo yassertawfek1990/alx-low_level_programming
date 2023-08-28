05b1452b01f85ca99bbaed06e7865743a7e6b697
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-28 14:35:46 -0700

    c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@
 
 char *_strchr(char *s, char c)
 {
-	int i
+	int i;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
@@ -18,5 +18,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
+
 	return ('\0');
 }
