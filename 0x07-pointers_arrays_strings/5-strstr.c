224f41c525f77402aa1509c29ffa7e685a87cdf1
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-28 17:27:03 -0700

    c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - returns the largest of 3 numbers
+ * @haystack: first intege
+ * @needle:d
+ * Return: largest number
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int x;
+	int l = strlen(needle);
+	int c = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (x = 0; needle[x] != '\0'; x++)
+		{
+			if (haystack[i + x] == needle[x])
+			{
+				c++;
+			}
+			else
+				break;
+		}
+		if (c == l)
+			return (haystack + i);
+		else
+			c = 0;
+	}
+	return ('\0');
+}
