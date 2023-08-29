31fc0ca48d426de890df0b780fbaad1b4d923883
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-28 18:39:30 -0700

    c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - returns the largest of 3 numbers
+ * @s: first intege
+ * to: d
+ * Return: largest number
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
