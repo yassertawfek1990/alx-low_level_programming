681153f45c40ab9d3f5c699ec981a5d5105a7c54
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 10:38:04 -0700

    c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the largest of 3 numbers
+ * @s: first intege
+ * Return: largest number
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = 0;
+
+	if (s != '\0')
+	{
+		n = n +  _strlen_recursion(s) + 1;
+	}
+	return (n);
+}
