ab314fb4802bcc7815e830ff8a442b4ff55a727b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 11:39:56 -0700

    c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the largest of 3 numbers
+ * @n: first intege
+ * Return: largest number
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	x = 1;
+
+	return (sq(n, x));
+}
+
+int sq(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	if (x * x < n)
+		return (sq(n, x + 1));
+	return (-1);
+}
