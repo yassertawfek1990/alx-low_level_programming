e531ec41f1f1642a31a8783ddc36fb7c04081b44
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 10:46:36 -0700

    c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the largest of 3 numbers
+ * @n: first intege
+ * Return: largest number
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+}
