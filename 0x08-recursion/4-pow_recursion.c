6d00ca4501685e422a85fa6998916862b34b520d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 11:01:20 -0700

    c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the largest of 3 numbers
+ * @x: first intege
+ * @y: d
+ * Return: largest number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	x = x * (_pow_recursion(x, y-1));
+	return (x);
+}
