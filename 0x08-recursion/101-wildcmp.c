8e5e37d6c6f06ffe0c4f37107c662a55eeb85054
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 18:37:23 -0700

    c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,14 +9,14 @@
 
 int wild(char *s1, char *s2, int i, int j) {
 
-  // Base cases
+  
   if (s2[j] == '\0')
     return s1[i] == '\0';
   
   if (i < 0 || j < 0)  
     return 0;
 
-  // Handle '*' wildcard
+  
   if (s2[j] == '*') {
     if (s1[i] == s2[j-1] && wild(s1, s2, i+1, j))
       return 1;
@@ -25,11 +25,11 @@ int wild(char *s1, char *s2, int i, int j) {
       return 1; 
   }
 
-  // Match current chars  
+ 
   if (s1[i] != s2[j])
     return 0;
 
-  // Recurse on remainder
+  
   return wild(s1, s2, i+1, j+1); 
 }
 
