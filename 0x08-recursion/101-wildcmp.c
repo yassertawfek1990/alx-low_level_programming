abfe6729532aa36ce3dcc1baca971d508c2d8354
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 18:33:58 -0700

    c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,19 +7,33 @@
  * Return: largest number
  */
 
-int wildcmp(char *s1, char *s2)
-{
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == '*')
-	{
-    if (wildcmp(s1, s2+1))
+int wildcmp(char *s1, char *s2, int i, int j) {
+
+  // Base cases
+  if (s2[j] == '\0')
+    return s1[i] == '\0';
+  
+  if (i < 0 || j < 0)  
+    return 0;
+
+  // Handle '*' wildcard
+  if (s2[j] == '*') {
+    if (s1[i] == s2[j-1] && wildcmp(s1, s2, i+1, j))
       return 1;
     
-    if (*s1 && wildcmp(s1+1, s2))  
-      return 1;
+    if (s1[i+1] == s2[j+1] && wildcmp(s1, s2, i, j+1))
+      return 1; 
   }
-	if (*s1 != *s2)
-		return (0);
-	return (wildcmp(s1 + 1, s2 + 2));
+
+  // Match current chars  
+  if (s1[i] != s2[j])
+    return 0;
+
+  // Recurse on remainder
+  return wildcmp(s1, s2, i+1, j+1); 
+}
+
+
+int wildcmp(char *s1, char *s2) {
+  return wildcmp(s1, s2, 0, 0); 
 }
---
+++ 0x08-recursion/dd.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * wildcmp - returns the largest of 3 numbers
+ * @s1: first intege
+ * @s2: d
+ * Return: largest number
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == '*')
+	{
+    if (wildcmp(s1, s2+1))
+      return 1;
+    
+    if (*s1 && wildcmp(s1+1, s2))  
+      return 1;
+  }
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 2));
+}
