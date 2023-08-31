b3899b2420c8a84a296206c7f7dfedaa4e5daaf7
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-08-30 18:26:13 -0700

    c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,7 +12,14 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '\0')
 		return (*s1 == '\0');
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	{
+ 		 while (*s1 != '\0') {
+    			if (wildcmp(s1, s2+1))
+     				 return 1;
+    			s1++;
+  		}
+ 	 return wildcmp(s1, s2+1);
+	}
 	if (*s1 != *s2)
 		return (0);
 	return (wildcmp(s1 + 1, s2 + 2));
