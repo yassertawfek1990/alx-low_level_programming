30fd17184865ec06e1ef05c92002924140dbb3f0
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-01 11:48:30 -0700

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ * @argc: f
+ * @argv: d
+ * Description: C program prints using printf command
+ * Return: always return 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
