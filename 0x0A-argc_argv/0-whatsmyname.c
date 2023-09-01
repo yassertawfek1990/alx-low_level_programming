506396ee37c9baa1176570056e1f051cfb07a76c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-01 11:38:59 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Enter program
+ * @argc: f
+ * @argv: d
+ * Description: C program prints using printf command
+ * Return: always return 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
