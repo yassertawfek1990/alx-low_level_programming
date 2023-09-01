8ffe67d14612f01ddae3fa3fee7c367fb2727ae8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-01 13:20:21 -0700

    c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,5 @@
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-  	int i;
-  
-  	if (argc == 1) 
-	{
-          	    printf("0\n");
-		    return 0;
-  	}
-  
-  	for (i = 1; i < argc; i++) 
-	{
-    		if (!isdigit(argv[i][0]))
-	       	{
-      			printf("Error\n");
-      			return 1;
-    		}
-    		sum += atoi(argv[i]);
-  	}
-  
-	printf("%d\n", sum);
-  
- 	return 0;
+	return 0;
 }
