1504192143047b375b4855774ce174b027197c06
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-04 11:11:59 -0700

    c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * create_array - returns the largest of 3 numbers
+ * @c: first intege
+ * @size: jw
+ * Return: largest number
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+	p = (char *)malloc((sizeof(char) * size));
+	*p = c;
+}
