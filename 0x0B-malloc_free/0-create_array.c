08732af9055ca97ec8636a5e3c784587f5b88875
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-04 11:37:09 -0700

    c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = (char *)malloc((sizeof(char) * size));
-	while (*p)
-	{	*p = c;
-		p++;
+	while (size--)
+	{	p[size] = c;
 	}
 
 	return (p);
