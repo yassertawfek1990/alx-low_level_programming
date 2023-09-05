123732555e9d7cddc3d7aaa79c38d5aa292509ce
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-05 14:36:32 -0700

    c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,45 +1,79 @@
 #include "main.h"
+/**
+ * strtow - returns the largest of 3 numbers
+ * @str: first intege
+ * Return: largest number
+ */
+char **strtow(char* str)
+{
+	int q;
+	int z;
+	int e;
+	int r;
+	int t;
+	int y;
+	int u;
+	char **p;
 
-char** strtow(char* str) {
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	y = d(str);
+	if (y == 1)
+		return (NULL);
+	p = (char**)malloc(y * sizeof(char*));
+	if (p == NULL)
+		return (NULL);
+	p[y - 1] = NULL;
+	q = 0;
+	while (str[q])
+	{
+		if (str[q] != 32 && (q == 0 || str[q - 1] == 32))
+		{
+			for (z = 1; str[q + z] != 32 && str[q + z]; z++)
+				;
+			z++;
+			p[u] = (char *)malloc(z * sizeof(char));
+			z--;
+			if (p[u] == NULL)
+			{
+				for (e = 0; e < u; e++)	
+					free(p[e]);
+				free(p[y - 1]);
+				free(p);
+				return (NULL);
+			}
+			for (r = 0; r < z; r++)
+				p[u][r] = str[q + r];
+			p[u][r] = '\0';
+			u++;
+			q = q + z;
+		}
+		else
+			q++;
+	}
+	return (p);
+}
+/**
+ * d - returns the largest of 3 numbers
+ * @a: first intege
+ * Return: largest number
+ */
+int d(char *a)
+{
+	int v;
+	int b;
 
-	int num_words, i, word_idx;
-	char ** words;
-	char* word;
-
-  if (str == NULL || *str == '\0') {
-    return NULL;
-  }
-
-  num_words = 0;
-  
-  for (i = 0; str[i]; i++) {
-    if (str[i] == ' ' && str[i+1] != '\0') {
-      num_words++;
-      while (str[i] == ' ') {
-        i++; 
-      }
-    }
-  }
-
-  num_words++;
-
-  words = malloc(sizeof(char*) * (num_words + 1));
-  if (!words) {
-    return NULL;
-  }
-
-  i = 0;
-  word_idx = 0;
-  word = strtok(str, " ");
-  while (word != NULL) {
-    words[word_idx] = strdup(word);
-    if (!words[word_idx]) {
-      return NULL; 
-    }
-    word = strtok(NULL, " ");
-    word_idx++;
-  }
-
-  words[word_idx] = NULL;
-  return words;
+	b = 0;
+	for (v = 0; a[v]; v++)
+	{
+		if (a[v] == 32)
+		{
+			if (a[v + 1] != 32 && a[v + 1] != '\0')
+				b++;
+		}
+		else if (v == 0)
+			b++;
+	}
+	b++
+	return (b);
 }
