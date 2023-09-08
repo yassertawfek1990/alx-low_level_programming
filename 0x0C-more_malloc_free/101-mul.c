af9f7527a394dde38b29f69d7cd09dba08dcfafe
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-07 19:10:42 -0700

    c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,100 +1,79 @@
 #include "main.h"
-
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
-
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char const *argv[])
 {
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
-	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
-	}
-	return (r);
-}
-
-
-
-int main(int argc, char **argv)
-{
-	char *r;
-	int a, c, x;
+	(void)argc;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		t("Error ");
+		exit(98);
 	}
-	if (!a)
-		_putchar('0');
+	it(ra(argv[1]) * ra(argv[2]));
 	_putchar('\n');
-	free(r);
 	return (0);
+}
+/**
+ * ra - check
+ * @c: aracter to check
+ * Return: 1
+ */
+int ra(const char *c)
+{
+	int g = 1;
+	unsigned long int m, w, x;
+
+	m = 0;
+	for (w = 0; !(c[w] >= 48 && c[w] <= 57); w++)
+	{
+		if (c[w] == '_')
+		{
+			g *= -1;
+		}
+	}
+	for (x = w; c[x] >= 48 && c[x] <= 57; x++)
+	{
+		m *= 10;
+		m += (c[x] - 48);
+	}
+	return (g * m);
+}
+/**
+ * it- strings
+ * @r: er string
+ * Return: thebigring
+ */
+void it(unsigned long int r)
+{
+	unsigned long int k, x, m;
+
+	k = 1;
+	for (x = 0; (r / k) > 9; x++, k *= 10)
+	;
+	for (; k >= 1; r %= k, k /= 10)
+	{
+		m = r / k;
+		_putchar('0' + m);
+	}
+}
+/**
+ * t- strings
+ * @f: er string
+ * Return: thebigring
+ */
+void t(char *f)
+{
+	int h;
+
+	h = 0;
+	while (f[h])
+	{
+		_putchar(f[h]);
+		h++;
+	}
 }
