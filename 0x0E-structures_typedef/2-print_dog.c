fba4db25cf9e1523b53ffa7aab8efd2e1cc841e3
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-11 14:35:03 -0700

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - returns the largest of 3 numbers
@@ -11,16 +12,16 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name = NULL)
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		else
-			printf("Name: %s", d->name);
+			printf("Name: %s\n", d->name);
 		if (d->age = NULL)
-        	        printf("Age: (nil)");
+        	        printf("Age: (nil)\n");
 		else
-			printf("Age: %d", d->age);
+			printf("Age: %d\n", d->age);
 		if (d->owner = NULL)
-        	        printf("Owner: (nil)");
+        	        printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
