60b2a58e331d8fb6ee830c51d70f096063cc6ad4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-11 13:11:04 -0700

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - returns the largest of 3 numbers
+ * @name: first intege
+ * @age: f
+ * @owner: dv
+ * Description: largest number
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif /* _DOG_H */
