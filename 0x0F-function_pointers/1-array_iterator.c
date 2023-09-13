37be2bbafc657fae272f1d48680a7e4f0f17c88c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-13 10:21:59 -0700

    c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - returns the largest of 3 numbers
+ * @array: first intege
+ * @size: c
+ * @action: ds
+ * Return: largest number
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
