bf0ca793796adace76716d59986c6bb484ef4193
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-13 12:57:21 -0700

    c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * main - Enter program
+ * Description: C program prints using printf command
+ * @argc: s
+ * @argv: cx
+ * Return: always return 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int n;
+	char *x = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	while (n--)
+		printf("02hhx%s", *x++, n ? " " : "\n");
+	return (0);
+}
