3861ed216eca34a911981008ea74d73d5e875395
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-13 10:31:57 -0700

    c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the largest of 3 numbers
+ * @array: first intege
+ * @size: c
+ * @cmp: ds
+ * Return: largest number
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1)
+}
