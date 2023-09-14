80a357f86701573bbe70d1e8895521ab5e859fbf
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-14 05:13:19 -0700

    c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,27 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int x;
 	va_list a;
 
+	x = n;
+
 	if (!n)
 	{
 		printf("\n");
 		return;
 	}
 	va_start(a, n);
-	for (x = 0; x < n; x++)
-	{
-		if (!separator)
-		{
-			printf("%d", va_arg(a, int));
-		}
-		else
-		{
-			if (x == 0)
-				printf("%d", va_arg(a, int));
-			else
-				printf("%s%d", separator, va_arg(a, int));
-		}
-	}
+	while (x--)
+		printf("%d%s", va_arg(a, int), x ? (separator ? separator : "") : "\n");
 	va_end(a);
-	printf("\n");
-
 }
