7b4c3ec3f6da59d876aea565c4a5e2fbdf4a5d9e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-21 11:26:12 -0700

    c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - returns the largest of 3 numbers
+ * @h: first intege
+ * Return: largest number
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
