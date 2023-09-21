ee03e95cd0baae51a659e0a9229dc5a508c6d1e7
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-21 14:03:24 -0700

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,6 +12,6 @@ void print_poem(void) __attribute__((constructor));
 
 void print_poem(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
