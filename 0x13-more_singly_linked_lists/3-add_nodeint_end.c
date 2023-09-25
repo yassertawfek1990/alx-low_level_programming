7e165d0dfd6e440e78d83a5bb2cca68a7b2ac6a6
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-25 14:21:42 -0700

    c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,9 +15,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	p = malloc(sizeof(listint_t));
 	if (!head || !p)
 		return (NULL);
-	if (t)
+	else
 	{
 		p->n = n;
+		p->next = NULL;
+	}
+	if (t)
+	{
 		while (t->next)
 			t = t->next;
 		t->next = p;
