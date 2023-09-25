970ba0a47e496ea169f2f0809ca34f23d3abbd7e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-25 15:42:00 -0700

    c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - returns the largest of 3 numbers
+ * @head: first intege
+ * Return: largest number
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *x;
+
+	while (head)
+	{
+		x = head->next;
+		free(head->n);
+		free(head);
+		head = x;
+	}
+}
