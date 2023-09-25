47fab10bc41e649b71e0317c3f023c3ec9ae5574
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-25 12:30:24 -0700

    c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - returns the largest of 3 numbers
+ * @head: first intege
+ * @n: dkd
+ * Return: largest number
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *p;
+
+	p = malloc(sizeof(listint_t));
+	if (!head || !p)
+		return (NULL);
+	if (n)
+	{
+		p->n = n;
+		if (!p->n)
+		{
+			free(p);
+			return (NULL);
+		}
+	}
+	p->next = *head;
+	*head = p;
+	return (p);
+}
