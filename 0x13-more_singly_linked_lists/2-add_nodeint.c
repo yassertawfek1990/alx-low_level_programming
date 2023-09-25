33320d1de120154937d838a30849f4cd9b2930fb
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-25 12:34:47 -0700

    c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *p;
 
 	p = malloc(sizeof(listint_t));
-	if (!head || !p)
+	if (!p)
 		return (NULL);
 	p->n = n;
 	p->next = *head;
