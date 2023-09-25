a954475733d435af9d622836b1b61ec1582b26a3
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-25 12:22:07 -0700

    c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - returns the largest of 3 numbers
+ * listint_len - returns the largest of 3 numbers
  * @h: first intege
  * Return: largest number
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t x = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		x++;
 	}
