e90f6cd23aaf9daf0341fcc4f330e23f52272aa8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-02 09:56:52 -0700

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,5 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	file *f;
+	char s[letters];
+
+	if (!filename)
+		return (0);
+	f = fopen(filename, r);
+	if (!f)
+		return (0);
+	else
+	{
+		if (fgets(filename, letters, f) != NULL)
+			return (strlen(f));
+		else
+			return (0);
+	}
+
+
 
 }
