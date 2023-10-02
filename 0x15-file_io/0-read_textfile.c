239f21faf89435e39b4f4889d9a3ffa813e70810
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-02 09:32:58 -0700

    c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text 
+ * @filename: A pointer to t
+ * @letters: The number of l
+ * Return: If the function fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
