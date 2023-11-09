d9e84943b48ef9353cbee75c12c03d4805b16f66
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-09 13:02:50 -0800

    c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,13 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *he;
+	dlistint_t *h;
 	while (head->prev != NULL)
 		head = head->prev;
 	while (head != NULL)
 	{
 		h = head->next;
 		free(head);
+		head = h;
 	}
 }
