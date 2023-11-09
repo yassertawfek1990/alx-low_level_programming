e9a00343d73561bf0dadc413c732cb864a6dc310
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-09 14:39:43 -0800

    c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,10 +9,9 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *t;
+	dlistint_t *t = *h;
 	dlistint_t *m;
 
-	t = *h;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	for (; idx != 1; idx--)
