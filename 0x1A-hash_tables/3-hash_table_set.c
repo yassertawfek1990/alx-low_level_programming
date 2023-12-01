0cc4fb607c8e921b7c6ae585ce92873d0aef2230
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-01 13:39:29 -0800

    c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[j]->key, key) == 0)
 		{
 			free(ht->array[j]->value);
-			ht->array[i]->value = v;
+			ht->array[j]->value = v;
 			return (1);
 		}
 	}
