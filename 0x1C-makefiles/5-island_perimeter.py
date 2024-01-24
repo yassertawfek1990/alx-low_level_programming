c59cb0920a30d2012af7535ce260a5580605170d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-01-24 21:05:43 +0200

    c

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define function"""
+
+
+def island_perimeter(grid):
+    """Returns island.
+
+    0 and land 1.
+
+    Args:
+        grid: An island.
+    Return:
+        The grid.
+    """
+    w= len(grid[0])
+    h = len(grid)
+    e = 0
+    s = 0
+    for x in range(h):
+        for c in range(w):
+            if grid[x][c] == 1:
+                s += 1
+                if (c > 0 and grid[x][c - 1] == 1):
+                    edges += 1
+                if (x > 0 and grid[x - 1][c] == 1):
+                    e += 1
+    return s * 4 - e * 2
